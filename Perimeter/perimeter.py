d9f7956c4e910e248e9644f6f7a9a69eca8d27d9
Vedant Datey <vedant.datey2001@gmail.com>
2020-11-19 19:59:57 +0530

    Changes to perimeter.py

--- Perimeter/perimeter.py
+++ Perimeter/perimeter.py
@@ -1,85 +1,86 @@
-class perimeter:
-    try:
-        def parallelogram(self, _base, _height):
-            '''
-            parallelogram(....)
-            parallelogram(self,_base,_height)
-             -> This returns the perimeter.py of parallelogram
-             -> accepts the two arguments:value of base and value of height
-            '''
-            return 2 * (_base + _height)
 
-        def triangle(self, _a=1, _b=1, _c=1, n=0):
-            '''
-            triangle(....)
-            triangle(self,_a,_b,_c)
-             -> This returns the perimeter.py of parallelogram
-             -> accepts the three arguments:values of three sides
-            '''
-            return _a + _b + _c
+'''This class gives the perimeter of basic shapes'''
+try:
+    def parallelogram(self, _base, _height):
+        '''
+        parallelogram(....)
+        parallelogram(self,_base,_height)
+         -> This returns the perimeter.py of parallelogram
+         -> accepts the two arguments:value of base and value of height
+        '''
+        return 2 * (_base + _height)
 
-        def rectangle(self, _length, _width):
-            '''
-            rectangle(....)
-            rectangle(self,_length,_width)
-             -> This returns the perimeter.py of rectangle
-             -> accepts the two arguments:values of length and width
-            '''
-            return 2 * (_length + _width)
+    def triangle(self, _a=1, _b=1, _c=1, n=0):
+        '''
+        triangle(....)
+        triangle(self,_a,_b,_c)
+         -> This returns the perimeter.py of parallelogram
+         -> accepts the three arguments:values of three sides
+        '''
+        return _a + _b + _c
 
-        def square(self, _side):
-            '''
-            square(....)
-            square(self,_side)
-             -> This returns the perimeter.py of square
-             -> accepts an argument: value of side
-            '''
-            return 4 * _side
+    def rectangle(self, _length, _width):
+        '''
+        rectangle(....)
+        rectangle(self,_length,_width)
+         -> This returns the perimeter.py of rectangle
+         -> accepts the two arguments:values of length and width
+        '''
+        return 2 * (_length + _width)
 
-        def trapezoid(self, _a, _b, _c, _d):
-            '''
-            trapezoid(....)
-            trapezoid(self,_a,_b,_c,_d):
-             -> This returns the perimeter.py of trapezoid
-             -> accepts the four arguments:values of its four sides
-            '''
-            return _a + _b + _c
+    def square(self, _side):
+        '''
+        square(....)
+        square(self,_side)
+         -> This returns the perimeter.py of square
+         -> accepts an argument: value of side
+        '''
+        return 4 * _side
 
-        def kite(self, _a, _b):
-            '''
-            kite(....)
-            kite(self,_a,_b):
-             -> This returns the perimeter.py of kite
-             -> accepts the two arguments:values of four sides
-            '''
-            return 2 * (_a + _b)
+    def trapezoid(self, _a, _b, _c, _d):
+        '''
+        trapezoid(....)
+        trapezoid(self,_a,_b,_c,_d):
+         -> This returns the perimeter.py of trapezoid
+         -> accepts the four arguments:values of its four sides
+        '''
+        return _a + _b + _c
 
-        def rhombus(self, _a):
-            '''
-            rhombus(....)
-            rhombus(self,_a):
-             -> This returns the perimeter.py of rhombus
-             -> accepts an argument:value of a side
-            '''
-            return 4 * _a
+    def kite(self, _a, _b):
+        '''
+        kite(....)
+        kite(self,_a,_b):
+         -> This returns the perimeter.py of kite
+         -> accepts the two arguments:values of four sides
+        '''
+        return 2 * (_a + _b)
 
-        def hexagon(self, _a):
-            '''
-            hexagon(....)
-            hexagon(self,_a):
-             -> This returns the perimeter.py of hexagon
-             -> accepts an argument:value of a side
-            '''
-            return 6 * _a
+    def rhombus(self, _a):
+        '''
+        rhombus(....)
+        rhombus(self,_a):
+         -> This returns the perimeter.py of rhombus
+         -> accepts an argument:value of a side
+        '''
+        return 4 * _a
 
-        def polygon(self, _n, _l):
-            '''
-            polygon(....)
-            rectangle(self,_n,_l):
-             -> This returns the perimeter.py of polygon
-             -> accepts the two arguments:number of sides and length of a side
-            '''
-            return _n * _l
-    except Exception as e:
-        print(e)
+    def hexagon(self, _a):
+        '''
+        hexagon(....)
+        hexagon(self,_a):
+         -> This returns the perimeter.py of hexagon
+         -> accepts an argument:value of a side
+        '''
+        return 6 * _a
+
+    def polygon(self, _n, _l):
+        '''
+        polygon(....)
+        rectangle(self,_n,_l):
+         -> This returns the perimeter.py of polygon
+         -> accepts the two arguments:number of sides and length of a side
+        '''
+        return _n * _l
+except Exception as e:
+    print(e)
 
